9b5fefbee0ea509714eb3ba18cc62fedd5b8467d
samandar92 <mavlonovsamandar1992@gmail.com>
2022-09-30 18:58:29 +0500

    zadachi photos

---
+++ zadachi photos/index.js
@@ -0,0 +1,31 @@
+// #1_return_sum
+
+// let num1 = +prompt("birinchi sonni kiriting")
+// let num2 = +prompt("ikkinchi sonni kiriting")
+// let num3 = prompt("+");
+
+// if(num3 == "+"){
+//     alert(num1 + num2);
+// }
+
+//#2_return_next_number
+
+// let num = +prompt("sonni kiriting")
+// if(num == num){
+//     alert(num + 1);
+// }
+// else{
+//     alert('sonni kiriting');
+// }
+
+
+//#3_convert_age_to_days
+
+let age = prompt("yoshingizni kiriting")
+
+function add(age) {
+    if(age = age){
+        alert(age * 365);
+    }
+}
+add(age)
