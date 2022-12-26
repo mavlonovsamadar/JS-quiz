c06ec0449b49512d0352105a2c228ac3535b80ea
samandar92 <mavlonovsamandar1992@gmail.com>
2022-12-26 22:18:04 +0500

    fixed

--- zadachi photos/index.js
+++ zadachi photos/index.js
@@ -1,17 +1,72 @@
-let obj = {
-    name: "Samandar",
-    age: 30,
-    food: "osh",
-    mine(){
-        let person = {
-            firstName: "Mavlonov",
-            address: "Olmaliq",
-            family(){
-                let arr = [6, 2, "Dadam", "ayam",  "ayolim", "farzandlarim"]
-                alert("Mening ismim " + obj.name + " Familyam " + person.firstName + ", Yoshim " + obj.age + "da. " + "Mening oilam " + arr[0] + " kishidan iborat. Ular: " + arr[2] + ", " + arr[3] + ", " + arr[4] + ", " + " va " + arr[1] + " nafar " + arr[5]);
-            }
-        }
-        person.family()
-    }
-}
-obj.mine()
+// Test-1
+// function getAge(...args) {
+//     return (typeof args)
+// }console.log(getAge(21));
+
+////////////////////////////////////////////////Test-2
+//sessionStorage.setItem('cool_secret', 123)
+
+////////////////////////////////////////////////Test-3
+// const user = {name:'lydia', age:21};
+// const admin = {admin:true, ...user};
+// console.log(admin);
+
+
+//Test-4
+// let c ={greeting: 'Hey!'};
+// let d;
+// d=c;
+// c.greeting = 'Hello'
+// console.log(d.greeting);
+
+
+//Test-5
+// function addtolist(item, list) {
+//     return list.push(item);
+// }
+// const result = addtolist('apple', ['banana']);
+// console.log(result);
+
+//Test-6
+// function Sayhi(name) {
+//     return `HI there, ${name}`;
+// }console.log(Sayhi());
+
+//////////////////////////////////////////////////////Test-7
+// function checkAge(age) {
+//     if(age<18){return alert( 'Sorry')}
+//     else{alert("Hi")}
+// }console.log(checkAge(21));
+
+//Test-8
+
+// fetch('https://www.website.com/api/user/1')
+// .then(res => res.json())
+// .then(res => console.log(res));
+
+////////////////////////////////////////////////Test-9
+
+// class Person {
+//     constructor(){
+//         this.name = 'Ldyia';
+//     }
+// }
+// Person = class AnotherPerson{
+//     constructor(){
+//         this.name = 'sarah';
+//     }
+// };
+// const member = new Person();
+// console.log(member.name);
+
+//Test-10
+
+// const ColorConfig = {
+//     red: true,
+//     blue: false,
+//     green: true,
+//     black: true,
+//     yellow: false,
+// };
+// const colors = ['pink', 'red', 'blue'];
+// console.log(ColorConfig.colors[0]);
